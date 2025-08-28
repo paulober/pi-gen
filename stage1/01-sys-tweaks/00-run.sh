f2199310a8adfe7fc5577be032a288d9d6310bd0
paulober <44974737+paulober@users.noreply.github.com>
2025-08-28 15:28:59 +0100

    Add cloud-init support
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -2,6 +2,9 @@
 
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
+# if cloud-init disabled
+if [[ "${ENABLE_CLOUD_INIT}" == "0" ]]; then
+
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
@@ -13,4 +16,4 @@ fi
 echo "root:root" | chpasswd
 EOF
 
-
+fi
